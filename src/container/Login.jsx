1179c9b506e02d501763ee1acf1dec3129c948c0
cpshibinoppo <cpshibinoppo@gmail.com>
2023-11-28 10:15:58 +0530

    chgeckj

--- src/container/Login.jsx
+++ src/container/Login.jsx
@@ -1,59 +1,58 @@
-import React, { useState } from "react"
-import { Form, redirect, useLoaderData, useNavigate } from "react-router-dom"
+import React from "react";
+import {
+  useLoaderData,
+  useNavigate,
+  Form,
+  redirect,
+//   useHistory,
+} from "react-router-dom";
 import { loginUser } from "../api";
-export function loader({request}){
-    return new URL(request.url).searchParams.get("message");
-}
-export async function action({request}){
-    const formData = await request.formData();
-    const email = formData.get("email");
-    const password = formData.get("password");
-    const data = await loginUser({email, password})
-    console.log(data)
-    localStorage.setItem('isLoggedIn', true)
-    return redirect('/');
+
+export function loader({ request }) {
+  return new URL(request.url).searchParams.get("message");
 }
 
+export async function action({ request }) {
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const data = await loginUser({ email, password });
+  console.log(data);
+  localStorage.setItem("loggedin", true)
+  return redirect("/host");
+}
 
 export default function Login() {
-    const [status , setStatus] = useState('idle');
-    const [error , setError] = useState(null);
-    const navigate = useNavigate();
-    const message = useLoaderData()
+  const [status, setStatus] = React.useState("idle");
+  const [error, setError] = React.useState(null);
+  const message = useLoaderData();
+  const navigate = useNavigate();
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        setStatus('submitting')
-        setError(null)
-        // loginUser(loginFormData)
-        // .then(data => { navigate('/host',{replace: true})})
-        // .catch(err => setError(err))
-        // .finally(()=>setStatus('idle'));
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    setError(null);
+    loginUser(loginFormData)
+      .then((data) => {
+        navigate("/host", { replace: true });
+      })
+      .catch((err) => setError(err))
+      .finally(() => setStatus("idle"));
+  }
 
+  return (
+    <div className="login-container">
+      <h1>Sign in to your account</h1>
+      {message && <h3 className="red">{message}</h3>}
+      {error && <h3 className="red">{error.message}</h3>}
 
-    return (
-        <div className="login-container">
-            {message && <h3 className="red">{message}</h3>}
-            <h1>Sign in to your account</h1>
-            <Form className="login-form" method="post">
-                <input
-                    name="email"
-                    type="email"
-                    placeholder="Email address"
-                />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                />
-                {error && <h3 className="red">{error.message}</h3>}
-
-                <button disabled={status === 'submitting'} >
-                {status === "submitting" ? "Logging in..." : "Log in"}
-                </button>
-            </Form>
-        </div>
-    )
-
+      <Form method="post" className="login-form" replace>
+        <input name="email" type="email" placeholder="Email address" />
+        <input name="password" type="password" placeholder="Password" />
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}
+        </button>
+      </Form>
+    </div>
+  );
 }
--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ import { redirect } from "react-router-dom"
 
 export async function requireAuth() {
     // const isLoggedIn = true
-    const isLoggedIn = localStorage.getItem('isLoggedIn')
+    const isLoggedIn = localStorage.getItem('loggedin')
     
     if (!isLoggedIn) {
         throw redirect("/login?message=You are not logged in")
